086a13902e13b1c3d93ae6da8fe876e427101efc
filon13p <filon13p@gmail.com>
2021-04-08 16:30:11 +0300

    *add key calculation in DataItem

--- src/main/java/lib/Hash/DataItem.java
+++ src/main/java/lib/Hash/DataItem.java
@@ -2,17 +2,34 @@ package lib.Hash;
 
 import com.sun.org.apache.xpath.internal.objects.XString;
 
+import static java.lang.Math.pow;
+
 public class DataItem {
-    private final int Key;
+    private int Key;
     public String Value;
 
     public int GetKey(){
         return this.Key;
     }
-
+    int calculateKey( int limit){
+        char[] KeyS = this.Value.toLowerCase().toCharArray();
+        int sum=0, i=0;
+        for (char c :KeyS){
+            int pow27 = (int) pow(27,i);
+            i++;
+            sum +=  ((int) c)*pow27;
+        }
+        sum %=limit;
+       return sum;
+    }
     //инициализатор
     {
         Value = "";
+    }
+    public DataItem(String value,int limit){
+        this.Value= value;
+        this.Key = calculateKey(limit);
+
     }
     public DataItem(int key){
         this.Key = key;
